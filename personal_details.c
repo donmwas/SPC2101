8c6a86725bc12b7bb35b3ed1d3778340e40f768e
donmwas <donmwas83@gmail.com>
2025-09-19 13:30:07 +0300

    Add files via upload

---
+++ personal_details.c
@@ -0,0 +1,25 @@
+//simple personal detail program
+/*
+Name:Donfan mwangi
+Reg no:PA106/G/29259/25
+Description:program to display personal details
+*/
+
+#include<stdio.h>
+
+int main(){
+int height;
+int networth;
+int age;
+int gender;
+    
+    printf("enter your height:");
+    scanf("%d",& height);
+    printf("enter your networth:");
+    scanf("%d",& networth);
+    
+    printf("enter your age:");
+    scanf("%d",& age);
+    printf("enter your gender:");
+    scanf("%d",& gender);
+    return 0;}
